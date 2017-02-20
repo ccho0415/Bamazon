58c8d48c87e5e514a09661a1b7f8d79a42e1f3dd
ccho0415 <ccho0415@gmail.com>
2017-02-20 14:03:10 -0500

    finished customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,28 +7,72 @@ var connection = mysql.createConnection({
 	database: "bamazon_db"
 });
 var orders = [];
-var ordernum = [];
-var num =0;
+var prices = [];
+var total = 0;
 function Order(order,num){
 	this.order=order,
 	this.num=num
 }
 function askOrder(){
-	num++	
-	inquirer.prompt([
+  	inquirer.prompt([
+		{
+			type:"list",
+			message: "Welcome to the Cattery! What would you like to order today?",
+			choices: ["Meow Mix","Blue Buffalo Cat", "Hills Science", "Fancy Feast", "Purina", "Bengal", "Tabby", "Scottish Fold", "Scratcher Toy", "Feather Wand", "Interactive Pet Paly Toy", "Cat Dancer", "Catit Senses 2.0 Digger for Cats","None","I'm Done"],
+			name: "order"
+		},
 		{
 			type: "prompt",
-			message: "How much of product "+ num +" would you like to buy?",
-			name: "num"
+			message: "How many would you like to buy?",
+			name: "num",
+			default: 0
 		}
-		]).then(function(user){
-			if (ordernum.length +1 <orders.length){
-				ordernum.push(user.num)
-				askOrder();
-			}else{
-				console.log(orders);
-			}
-		});		
+	]).then(function(user){
+		var input = user.order;
+		var purchased = user.num;
+		if(input !== "None" && input !="I'm Done"){
+			connection.query("SELECT*FROM products WHERE product_name LIKE '"+user.order+"'", function(error, results, fields){
+				if (error) throw error;
+				else if (results){
+					console.log(results);
+					var currentNum =parseInt(results[0].stock_quantity);
+					var currentPrice =parseInt(results[0].price)* parseInt(user.num);
+					var productId =parseInt(results[0].id);
+					prices.push(currentPrice);
+					var newNum = currentNum -1
+					if (newNum >0){
+
+					console.log(currentNum);
+					console.log(newNum);
+					connection.query("UPDATE products SET stock_quantity = '"+ newNum +"' WHERE product_name = '"+user.order+"'", function(error, results, fields){
+						if (error) throw error;
+						else if(results){
+							connection.query("INSERT INTO sales (product_id,quantity_purchased) VALUES ('"+productId+"','"+ purchased +"')");
+							console.log("That will be "+ currentPrice+ " dollars" );
+
+						}
+					});	
+					}else if (newNum <0){
+						console.log("-----We ran out!-----");
+					}
+				}
+			});
+			var orderData = new Order(user.order, user.num);
+			orders.push(orderData);
+			askOrder();
+		}else if (input == "I'm Done"){
+			prices.forEach(function(element){
+				var addThis =parseInt(element);
+				total = total + addThis;
+
+			});
+			console.log("Your total came out to "+ total+ " dollars. Thank you for shopping at the Cattery.");
+			process.exit();
+		}else if (input =="None"){
+			console.log("you suck");
+			process.exit();
+		}
+	});	
 
 }
 connection.connect(function(err) {
@@ -43,20 +87,6 @@ connection.query('SELECT * FROM products', function (error, results, fields) {
   if (error) throw error;
   else if(results){
   	console.log(results);
-  	inquirer.prompt([
-		{
-			type:"checkbox",
-			message: "Welcome to the Cattery! What would you like to order today?",
-			choices: ["Meow Mix","Blue Buffalo Cat", "Hill's Science", "Fancy Feast", "Purina", "Bengal", "Tabby", "Scottish Fold", "Scratcher Toy", "Feather Wand", "Interactive Pet Paly Toy", "Cat Dancer", "Catit Senses 2.0 Digger for Cats"],
-			name: "order"
-		}
-	]).then(function(user){
-		var input = user.order
-		input.forEach(function(element){
-			orders.push(element);
-		});
-		askOrder();
-		var orderData = new Order()
-	});
+  	askOrder();
   }
 });
